ee091ad53806ad79febc6e516e3b2fd5c439b674
luislp <luislp777177@gmail.com>
2021-11-10 00:22:54 -0500

    Agrege operacion del imc

--- imc.py
+++ imc.py
@@ -1,2 +1,8 @@
 masa=float(input("Ingresa tu masa en kilogramos"))
-peso=float(input("Ingresa tu masa en kilogramos"))
+peso=float(input("Ingresa tu altra en metros"))
+
+imc=masa/altura**2
+
+
+imc=masa/altura**2
+
